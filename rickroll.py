37709d3bbc940c6f9512c846ee0ae31641f0692e
Krish Penumarty <krishpenumarty@yahoo.com>
2016-08-24 10:22:25 -0700

    Added a print statement to show you the time when the video will play in minutes.

--- rickroll.py
+++ rickroll.py
@@ -6,6 +6,7 @@ import random
 i = 0
 while i < 3:
     randomTime = random.randint(10, 20)
+    print randomTime
     time.sleep(randomTime * 60)
     webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     i = i + 1
